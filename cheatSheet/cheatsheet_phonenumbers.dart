570f994fb1185a8cf60f43649d3a28baacbed337
atligudna <atligudna@gmail.com>
2025-02-20 23:19:20 +0000

    Tími 2 breytingar

---
+++ cheatSheet/cheatsheet_phonenumbers.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  bool validNumber = false;
+  while (validNumber == false) {
+    print("Enter your Phone number.");
+    String phoneNumber = stdin.readLineSync().toString();
+    if (phoneNumber.contains("+354")) {
+      phoneNumber = phoneNumber.replaceAll("+354", "");
+      print ("Þú skrifaðir $phoneNumber, ég tek +354 út.");
+    }
+    if (phoneNumber.length == 8) {
+      // if(phoneNumber.contains("-")) {
+      // print("Þetta símanúmer er með -");
+      // }
+      phoneNumber = phoneNumber.replaceAll("-", "");
+      phoneNumber = phoneNumber.replaceAll(" ", "");
+      print("Þú skrifaðir $phoneNumber, ég tek út - og bil.");
+    }
+    int? phoneNumberStorage = int.tryParse(phoneNumber);
+    if (phoneNumber.length == 7 && phoneNumberStorage is int) {
+      if (phoneNumber[0] != "1") {
+        print("þetta símanúmer er valid");
+        validNumber = true;
+      }
+    }
+  }
+}
