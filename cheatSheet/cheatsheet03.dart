cbe29a891b669635bc4e160c1f150f0c2362e09e
atligudna <atligudna@gmail.com>
2025-02-25 21:43:31 +0000

    Create cheatsheet03.dart

---
+++ cheatSheet/cheatsheet03.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  // Number guessing game
+
+  print("Welcome to Number game.");
+
+  int randomNumber = Random().nextInt(100) + 1;
+  //print("The random Number is $randomNumber");
+
+  bool rightNumber = true;
+
+  while (rightNumber == true) {
+    print("Guess a number between 1 og 100");
+    String? input = stdin.readLineSync();
+    //print(input);
+    int inputNumber = int.parse(input!);
+
+    int diff = (inputNumber - randomNumber).abs();
+    print(diff);
+
+    if (inputNumber == randomNumber) {
+      print("Congratulation. You were right.");
+      rightNumber = false;
+    }
+    if (inputNumber < randomNumber && diff > 10){
+      print("Your number is way too low. Try again");
+    }
+    if (inputNumber < randomNumber && diff <= 10){
+      print("Your number is too low. Try again");
+    }
+    if (inputNumber > randomNumber && diff > 10){
+      print("Your number is way too high. Try again");
+    }
+    if (inputNumber >= randomNumber && diff <= 10){
+      print("Your number is too high. Try again");
+    }
+  }
+}
